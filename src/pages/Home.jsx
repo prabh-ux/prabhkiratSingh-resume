414535f2f9369b71921dcdee4246ca0446adbe54
prabh-ux <prabhkiratsinghbusiness@gmail.com>
2025-03-08 10:37:30 +0530

    Fixed react-icons import issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,7 @@ import CardButton from '../components/cardButton'
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa6";
-import BacktoTopbtn from '../components/BacktoTopbtn';
+
 import mypic from '../images/me.gif';
 const Home = () => {
 
