f68c7f27b8fd15a0905f44861ea9a1b6a9a6f1e4
prabh-ux <prabhkiratsinghbusiness@gmail.com>
2025-03-08 11:46:32 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   base: './', // Ensures correct asset paths on Vercel
   build: {
     outDir: 'dist',
